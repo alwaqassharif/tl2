cbd631ca300b6f63b7983b2f4d82cb13c4343185
alwaqassharif <66223353+alwaqassharif@users.noreply.github.com>
2026-05-01 04:24:09 -0700

    Update posts-data.js

--- posts-data.js
+++ posts-data.js
@@ -1,16 +1,16 @@
-// blogs.js
+ // posts-data.js
 
-const BLOGS = [
-  { title: "Best guest posting service 2026", link: "/best-guest-posting-service.html", category: "guest-posting" },
-  { title: "Guest Posting Outreach", link: "/guest-posting-outreach.html" , category: "guest-posting"},
-  { title: "Guest Post link building", link: "/guest-post-link-building.html", category: "guest-posting" },
-  { title: "Blogs that accept guest posts", link: "/blogs-that-accept-guest-posts.html", category: "guest-posting" },
-  { title: "Where to get guest posts", link: "/where-to-get-guest-posts-for-addiction-center.html" , category: "guest-posting"},
-  { title: "High authority guest posts", link: "/high-authority-guest-posts.html" , category: "guest-posting"},
-  { title: "Real estate guest posts", link: "/real-estate-guest-post.html" , category: "guest-posting"},
-  { title: "Technology guest posts", link: "/technology-guest-post.html", category: "guest-posting" },
-  { title: "Sports guest posts", link: "/sports-guest-post.html" , category: "guest-posting"},
-  { title: "White label guest posts", link: "/white-label-guest-posts.html" , category: "guest-posting"},
-  { title: "Guest posts Vs niche edits", link: "/guest-posting-vs-niche-edits.html" , category: "guest-posting"},
-  { title: "Dofollow guest post guide", link: "/dofollow-guest-post-guide.html" , category: "guest-posting"}
+const POSTS_DATA = [
+  { title: "Best guest posting service 2026", link: "/best-guest-posting-service.html" },
+  { title: "Guest Posting Outreach", link: "/guest-posting-outreach.html" },
+  { title: "Guest Post link building", link: "/guest-post-link-building.html" },
+  { title: "Blogs that accept guest posts", link: "/blogs-that-accept-guest-posts.html" },
+  { title: "Where to get guest posts", link: "/where-to-get-guest-posts-for-addiction-center.html" },
+  { title: "High authority guest posts", link: "/high-authority-guest-posts.html" },
+  { title: "Real estate guest posts", link: "/real-estate-guest-post.html" },
+  { title: "Technology guest posts", link: "/technology-guest-post.html" },
+  { title: "Sports guest posts", link: "/sports-guest-post.html" },
+  { title: "White label guest posts", link: "/white-label-guest-posts.html" },
+  { title: "Guest posts Vs niche edits", link: "/guest-posting-vs-niche-edits.html" },
+  { title: "Dofollow guest post guide", link: "/dofollow-guest-post-guide.html" }
 ];
