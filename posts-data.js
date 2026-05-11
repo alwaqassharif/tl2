d503d6a27af7f4340281184fae8e30679d909f2c
alwaqassharif <66223353+alwaqassharif@users.noreply.github.com>
2026-05-11 10:35:32 -0700

    Update posts-data.js

--- posts-data.js
+++ posts-data.js
@@ -26,8 +26,8 @@ const POSTS_DATA = [
 { title: "Link building for new Shopify website", link: "/link-building-for-new-shopify-website.html", category: "guest-posting" },
 { title: "Local SEO Guest Posting Strategy", link: "/local-seo-guest-posting-strategy.html", category: "guest-posting" },
 { title: "Content Writing for Guest Posts", link: "/content-writing-for-guest-posts.html", category: "guest-posting" },
-{ title: "Health blogs that accept guest posts", link: "/health-blogs-that-accept-guest-posts.html", category: "guest-posting" }
-
+{ title: "Health blogs that accept guest posts", link: "/health-blogs-that-accept-guest-posts.html", category: "guest-posting" },
+{ title: "Broken Link Building Strategy", link: "/broken-link-building-strategy.html", category: "guest-posting" }
 ];
 
 
