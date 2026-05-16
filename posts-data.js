18fda1d782fb0fee1713f82ccf52b72823c5477f
alwaqassharif <66223353+alwaqassharif@users.noreply.github.com>
2026-05-15 21:40:32 -0700

    Update posts-data.js

--- posts-data.js
+++ posts-data.js
@@ -36,6 +36,9 @@ const POSTS_DATA = [
 { title: "How Many Backlinks Per Guest Post Is Safe?", link: "/how-many-backlinks-per-guest-post-is-safe.html", category: "guest-posting" },
 { title: "Guest post topic research method step by step", link: "/guest-post-topic-research-method-step-by-step.html", category: "guest-posting" },
 { title: "Guest post link decay", link: "/guest-post-link-decay.html", category: "guest-posting" },
-{ title: "Guest posting ROI calculation method", link: "/guest-posting-roi-calculation-method.html", category: "guest-posting" }
+{ title: "Guest posting ROI calculation method", link: "/guest-posting-roi-calculation-method.html", category: "guest-posting" },
+{ title: "How to qualify websites for guest posting", link: "/how-to-qualify-websites-for-guest-posting.html", category: "guest-posting" }
+
+
 ];
 	
