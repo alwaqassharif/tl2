ae46baf3b08834470aa87c2700654c425595916b
alwaqassharif <66223353+alwaqassharif@users.noreply.github.com>
2026-05-16 12:24:56 -0700

    Update posts-data.js

--- posts-data.js
+++ posts-data.js
@@ -38,7 +38,8 @@ const POSTS_DATA = [
 { title: "Guest post link decay", link: "/guest-post-link-decay.html", category: "guest-posting" },
 { title: "Guest posting ROI calculation method", link: "/guest-posting-roi-calculation-method.html", category: "guest-posting" },
 { title: "How to qualify websites for guest posting", link: "/how-to-qualify-websites-for-guest-posting.html", category: "guest-posting" },
-{ title: "Guest post pricing per domain authority explained", link: "/guest-post-pricing-per-domain-authority-explained.html", category: "guest-posting" }
+{ title: "Guest post pricing per domain authority explained", link: "/guest-post-pricing-per-domain-authority-explained.html", category: "guest-posting" },
+{ title: "Pros and cons of blogging", link: "/pros-and-cons-of-blogging.html", category: "guest-posting" }
 
  ];
 	
